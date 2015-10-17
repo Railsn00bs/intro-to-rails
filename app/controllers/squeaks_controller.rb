9a4e4a6b1d9c16fa7345d3832ac053c630f29b42
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-17 15:49:15 +1300

    Squeaks are created by users

--- app/controllers/squeaks_controller.rb
+++ app/controllers/squeaks_controller.rb
@@ -1,11 +1,13 @@
 class SqueaksController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   end
 
   def create
     Squeak.create!(
       message: params[:squeak][:message],
-      user: User.first
+      user: current_user
     )
 
     redirect_to action: :index
