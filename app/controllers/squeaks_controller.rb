74e1fe189d1ca012dbccefc5f28293f762c18568
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-17 15:34:10 +1300

    Add a form to create squeaks!

--- app/controllers/squeaks_controller.rb
+++ app/controllers/squeaks_controller.rb
@@ -1,4 +1,13 @@
 class SqueaksController < ApplicationController
   def index
   end
+
+  def create
+    Squeak.create!(
+      message: params[:squeak][:message],
+      user: User.first
+    )
+
+    redirect_to action: :index
+  end
 end
