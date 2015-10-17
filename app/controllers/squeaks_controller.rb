fa7e5fad6f8557ef7b0bc2f19cfe05a68c697190
Nick Johnstone <ncwjohnstone@gmail.com>
2015-10-17 15:18:28 +1300

    Display a list of squeaks

--- app/controllers/squeaks_controller.rb
+++ app/controllers/squeaks_controller.rb
@@ -1,2 +1,4 @@
 class SqueaksController < ApplicationController
+  def index
+  end
 end
---
+++ app/views/squeaks/monster-vim-2015-10-17-15-15-20.rb
@@ -0,0 +1,9 @@
+<div>Hello sploorld</div>
+
+
+<ul>
+<% Squeak.all.each do |squeak| %>
+  <%= squeak.message %>
+<% end %>
+</
+
